aeb6dc1221d34486d80eb254f123e87c2712567f
Taejin Jung <taejin5314@gmail.com>
2021-07-13 18:40:00 +0000

    change __ to -

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,11 +4,81 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const avatars = {
+$(document).ready(function() {
+  const avatars = {
+      
+    Female: ["https://i.imgur.com/nlhLi3I.png","https://i.imgur.com/z5LNkkB.png","https://i.imgur.com/v0JXau2.png","https://i.imgur.com/lRUnDgU.png", "https://i.imgur.com/3GvwNBf.png"],
+    Male: ["https://i.imgur.com/73hZDYK.png","https://i.imgur.com/5fUVPRP.png","https://i.imgur.com/DVpDmdR.png","https://i.imgur.com/2WZtOD6.png", "https://i.imgur.com/ilT4JDe.png"]
+  
+  }
+  
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
     
-  Female: ["https://i.imgur.com/nlhLi3I.png","https://i.imgur.com/z5LNkkB.png","https://i.imgur.com/v0JXau2.png","https://i.imgur.com/lRUnDgU.png", "https://i.imgur.com/3GvwNBf.png"],
-  Male: ["https://i.imgur.com/73hZDYK.png","https://i.imgur.com/5fUVPRP.png","https://i.imgur.com/DVpDmdR.png","https://i.imgur.com/2WZtOD6.png", "https://i.imgur.com/ilT4JDe.png"]
+      $('#tweets-container').append($tweet);
+    }
+  }
+  
+  const createTweetElement = function(tweet) {
+    let $tweet = $(`
+      <article class="tweet">
+        <header class="tweet-header">
+          <img src="${tweet.user.avatars}" alt="face">
+          <p>${tweet.user.name}</p>
+          <span>${tweet.user.handle}</span>
+        </header>
+        <div class="tweet-content">
+          <p>${tweet.content.text}</p>
+        </div>
+        <footer class="tweet-footer">
+          <p>${timeago.format(tweet.created_at)}</p>
+          <div class="tweet-icons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+      `);
+  
+    return $tweet; 
+  }
 
-}
+  // when new tweet submitted
+  $('.container-form').submit(function(event) {
+    event.preventDefault();
+    const formValue = $(this).serialize();
 
-const $tweet = $(`<article class="tweet">Hello world</article>`);
+    $.post('tweets', formValue, function(data) {
+        console.log(data.text);
+      }, 'json')
+  })
+  
+  renderTweets(data);
+});
