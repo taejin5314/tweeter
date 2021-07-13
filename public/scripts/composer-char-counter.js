a38cb4deb6903521c8b203776d8a2180fd6c86e4
Taejin Jung <taejin5314@gmail.com>
2021-07-13 00:03:38 +0000

    new tweet textarea count (red negative, black positive)

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('#tweet-text').on('input', function() {
+    const maxLength = 140;
+    const textLength = $(this).val().length;
+    const count = maxLength - textLength;
+    const countSelector = $($(this).parent().children()[2]).children()[1];
+    
+    if (count < 0) {
+      $(countSelector).css('color', '#FF0000');
+    } else {
+      $(countSelector).css('color', '#000000')
+    }
+    $(countSelector).text(count);
+  })
+})
