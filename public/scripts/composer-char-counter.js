082b5a71ccb6cdd6193534146fe302122693660b
Taejin Jung <taejin5314@gmail.com>
2021-07-13 23:55:29 +0000

    fix the color problem

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
     if (count < 0) {
       $($countSelector).css('color', '#FF0000');
     } else {
-      $($countSelector).css('color', '#000000');
+      $($countSelector).css('color', '#545149');
     }
     $($countSelector).text(count);
   });
