c0f77943832d5be877452a562d714c8212871bec
Taejin Jung <taejin5314@gmail.com>
2021-07-14 15:29:58 +0000

    destructuring

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
+  // reset the count when the form is submitted
   $('#tweet-text').on('change', function() {
     const $countSelector = $($(this).parent().children()[2]).children()[1];
     $($countSelector).text(140);
   });
+
+  // count the character every time user types
   $('#tweet-text').on('input', function() {
     const maxLength = 140;
     const textLength = $(this).val().length;
